d5e1b2f4dcb56e018dcb3adedf1e31f686e45b86
Pawee Kittiwathanakul <boatmind2710@gmail.com>
2019-09-16 16:01:27 +0700

    create project

---
+++ assigment/assigment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assigment
+//
+//  Created by Pawee Kittiwathanakul on 16/9/2562 BE.
+//  Copyright © 2562 Pawee Kittiwathanakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
